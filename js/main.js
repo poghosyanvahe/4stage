59c7d2a53349be3fda3ad4fa5547c0362c28d140
vahe poghosyan <vahe@arattauna.com>
2016-06-21 19:06:02 +0400

    qwerty

--- js/main.js
+++ js/main.js
@@ -7,13 +7,56 @@ $(document).ready(function () {
 
     });
 
-    $('.navbar-nav').find('li').on('click',function (e) {
-        e.preventDefault();
-        $(this).parent().find('li a').css('color','#333');
-        $(this).find('a').css('color','#999');
 
-        
+});
+
+///drop menu show hide
+
+$(document).on('click', function (e) {
+    if ($('.drop-menu').is(':visible') && $(e.target).closest(".drop-menu").length == 0 && $(e.target).closest(".navbar-nav").length == 0) {
+        $('.drop-menu').removeClass('active');
+        $('.navbar-nav').find('li').removeClass('active')
+    }
+
+    if ($('.login').is(':visible') && $(e.target).closest(".login").length == 0) {
+        $('.login').fadeOut();
+        if ($('main>div').hasClass('black-overlay')) {
+            $('main>div').removeClass('black-overlay');
+        }
+
+    } else if (!$('.login').is(':visible') && $(e.target).closest(".user-login").length != 0) {
+        $('.login').fadeIn();
+        if (!$('main>div').hasClass('black-overlay')) {
+            $('main>div').addClass('black-overlay');
+            $('.drop-menu.active').hide();
+        }
+
+    }
+
+
+    if ($('.subtotal').is(':visible') && $(e.target).closest(".subtotal").length == 0) {
+        $('.subtotal').fadeOut();
+        if ($('main>div').hasClass('black-overlay')) {
+            $('main>div').removeClass('black-overlay');
+        }
+    } else if (!$('.subtotal').is(':visible') && $(e.target).closest(".bucket").length != 0) {
+        $('.subtotal').fadeIn();
+        if (!$('main>div').hasClass('black-overlay')) {
+            $('main>div').addClass('black-overlay');
+        }
+    }
+
+});
+
+/// END drop menu show hide
+
+
+$('.bucket + li').text($('.subtotal').find('li').length);
+$('.close').on('click', function (e) {
+    console.log(123);
+    $(this).parents().eq(1).fadeOut(200, function (e) {
+        $(this).remove();
+        $('.bucket + li').text($('.subtotal').find('li').length)
     });
-
 
 });
